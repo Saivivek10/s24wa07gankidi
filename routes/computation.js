df7c58c50965f34b04d68bf71eeb3a98a554b613
Sai Vivek Reddy Gankidi <saivivekreddy100@gmail.com>
2024-03-20 21:03:22 -0500

    new endpoint

--- routes/computation.js
+++ routes/computation.js
@@ -8,7 +8,8 @@ router.get('/', function(req, res, next) {
   const x = req.query.x ? parseFloat(req.query.x) : randomNumber;
  
  
- // res.send(`Math.cos() of ${x} is ${Math.cos(x)}<br> Math.asin() applied to ${x} is ${Math.asin(x)}<br> Math.asinh() applied to ${x} is ${Math.asinh(x)}`);
- res.send(`Math.atan() of ${x} is ${Math.atan(x)}<br> Math.exp() applied to ${x} is ${Math.exp(x)}<br> Math.expm1() applied to ${x} is ${Math.expm1(x)}`);
+  res.send(`Math.cos() of ${x} is ${Math.cos(x)}<br> Math.asin() applied to ${x} is ${Math.asin(x)}<br> Math.asinh() applied to ${x} is ${Math.asinh(x)}`);
+  module.exports = router;
+ 
  
 });
